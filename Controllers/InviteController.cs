a32b104a0d042cfcd1edf8d189e70434a6472ae9
0x0000eWan <xpodsednikm@gmail.com>
2015-05-21 20:21:07 +0200

    m

--- Controllers/InviteController.cs
+++ Controllers/InviteController.cs
@@ -183,7 +183,29 @@ namespace ebis.Controllers
                 mail.From = new MailAddress("ebis.office@gmail.com"); // office@eb-is.cz
                 mail.Subject = "Pozvánka";
                 string content = oa.akce_id.ToString() + "-" + oa.nastroje_id.ToString() + "-" + oa.osoby_id.ToString(); // localhost:52663
-                string Body = 
+                var cislo_smlouvy = oa.osoby.smlouvy.Any() ? oa.osoby.smlouvy.Last().id : 0;
+                string nazev_akce = oa.akce.popis;
+                string Body = "<h1>NÁBÍDKA a POTVRZENÍ ÚČASTI NA PROJEKTU na základě smlouvy č." + cislo_smlouvy.ToString() + "</h1><br>" +
+                    "Já, " + oa.osoby.jmeno + " " + oa.osoby.prijmeni + ", narozen: " + oa.osoby.datum_narozeni.ToString() + "<br>" +
+                    "Potvrzuji tímto svoji účast na projektu:<br>" +
+                    nazev_akce + "<br>" +
+                    "<ul>" +
+                    "<li>Termín vystoupení: " + oa.akce.datum + ", " + oa.akce.cas + "</li>" +
+                    "<li>Program: " + oa.akce.program + "</li>" +
+                    "<li>Výše honoráře: " + oa.honorar.ToString() + " Kč plus náklady na dopravu, které se zavazuji doplnit do 7 dnů od přijetí této nabídky, jinak na proplacení mohu ztratit nárok." + "</li>" +
+                    "<li>Funkce: " + oa.nastroje.jmeno + "</li>" +
+                    "<li>Obsazení projektu: ..." + "</li>" +
+                    "<li>Zvláštní podmínky: ..." + "</li>" +
+                    "<li>Poznámka: ..." + "</li>" +
+                    "<li>Předběžný harmonogram projektu:</li>" +
+                    "</ul>" +
+                    "<div>" +
+
+                    "</div>" +
+                    "<p>Beru na vědomí, že předpokládaný ferman se může vlivem nepředvídatelných událostí změnit a že na základě své Smlouvy nebudu v uvedeném termínu plánovat žádné další aktivity, které by mi neumožnili zúčastnit se zkoušek a vystoupení.</p>" +
+                    "Prosíme, zareagujte včas na naši nabídku a obratem zašlete svou odpověď, zda máte, nebo nemáte o projekt zájem. V případě, že se můžete projektu v celém rozsahu zúčastnit, klikněte na zelené tlačítko a pokračujte dle instrukcí na webové stránce, na kterou budete přesměrováni. " +
+                    "V případě, že se projektu můžete zúčastnit a máte jen drobnou časovou kolizi (max. 1 zkušební frekvence), klikněte na modré tlačítko a pokračujte dle instrukcí na webové stránce, na kterou budete přesměrováni. Máte-li větší časové kolize, či nemáte-li o projekt zájem, klikněte " +
+                    "na oranžové tlačítko tlačítko. Formulář se obratem odešle zpět, stane se součásti Vaší roční smlouvy č." + cislo_smlouvy.ToString() + ", a nabývá tak právní hodnoty podepsané listiny." + 
                     "<div><a href=\"http://www.eb-is.cz/Invite/Response/?command=" + Encrypt(content + "-1") + "\"><img src=\"http://www.eb-is.cz/Images/accept.png\"></a>" +
                     "<a href=\"http://www.eb-is.cz/Invite/Response/?command=" + Encrypt(content + "-2") + "\"><img src=\"http://www.eb-is.cz/Images/decline.png\"></a>" +
                     "<a href=\"http://www.eb-is.cz/Invite/Response/?command=" + Encrypt(content + "-3") + "\"><img src=\"http://www.eb-is.cz/Images/accept2.png\"></a></div>";
